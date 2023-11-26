e518d3f7d66a52de72249a17bb97b7d8f93a56bd
Wisdom <eluewisdom@gmail.com>
2023-11-26 21:54:37 +0100

    feat: get all books api done

--- server/handlers/book.handler.go
+++ server/handlers/book.handler.go
@@ -1,1 +1,17 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/elue-dev/bookVerse/controllers"
+	"github.com/elue-dev/bookVerse/helpers"
+)
+
+func GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	result, err := controllers.GetBooks()
+	if err != nil {
+		helpers.SendErrorResponse(w, 500, "Something went wrong while fetching books",  err)
+	}
+
+	helpers.SendSuccessResponseWithData(w, 200, result)
+}
--- server/router/router.go
+++ server/router/router.go
@@ -8,10 +8,15 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
+	// HEALTH CHECK ROUTE
 	router.HandleFunc("/api/healthz", handlers.CheckServerHealth).Methods("GET", "OPTIONS")
 
+	// AUTH ROUTES
 	router.HandleFunc("/api/auth/signup", handlers.SignUp).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/auth/login", handlers.Login).Methods("POST", "OPTIONS")
+
+	// BOOK ROUTES
+	router.HandleFunc("/api/books", handlers.GetAllBooks).Methods("GET", "OPTIONS")
 
 	return router
 }
